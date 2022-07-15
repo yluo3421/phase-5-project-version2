ddb9f40e743ac3972bb37d9aacd11d4ecd8970ee
omar3o3 <otantawy53@gmail.com>
2022-07-15 15:00:46 -0400

    copied stuff from creatingComments

--- app/serializers/user_event_serializer.rb
+++ app/serializers/user_event_serializer.rb
@@ -2,4 +2,5 @@ class UserEventSerializer < ActiveModel::Serializer
   attributes :id, :event_id, :event_location, :event_name, :start_date_time, :end_date_time, :event_borough, :event_type
 
   belongs_to :user
+  has_many :comments
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,4 +2,5 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :actual_name, :username, :password, :image_url
 
   has_many :user_events
+  has_many :comments
 end
